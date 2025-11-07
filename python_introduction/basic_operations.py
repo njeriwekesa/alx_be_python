ed99ba006e91d52ea25f00fa2e002d4dce57f6b6
Charity Njeri <wekesacharity@gmail.com>
2025-11-07 14:01:42 +0300

    Add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 6
+number2 = 7
+sum = number1 + number2
+difference = number2 - number1
+
+
+print("Addition of", number1, "and", number2, "is", sum)
+
+print("Subtraction of", number1, "and", number2, "is", difference)
+
