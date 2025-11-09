3fa0783d1d7c2a9f360d92c2f7385af6d3a0b7fc
Charity Njeri <wekesacharity@gmail.com>
2025-11-09 10:03:17 +0300

    Update finance_calculator.py with dollar signs in output

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,5 +3,6 @@ expenses = int(input("Enter your monthly expenses: "))
 savings = income - expenses 
 projectedAnnualSavings = savings * 12 + (savings * 12 * 0.05)
 
-print(savings)
-print("Projected savings, after one year, with interest is:", projectedAnnualSavings)
+print(f"Your monthly savings are ${savings}.")
+print(f"Projected savings after one year, with interest, is: ${projectedAnnualSavings}.")
+
