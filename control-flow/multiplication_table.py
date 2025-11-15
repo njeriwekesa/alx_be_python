a7c53d21ae3f12fa672da33aec493bf7c5595d17
Charity Njeri <wekesacharity@gmail.com>
2025-11-15 04:51:37 +0300

    Add multiplication_table.py

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+# This script generates a multiplication table for a given number
+
+number = int(input("Enter a number to see its multiplication table: "))
+
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} * {i} = {result}")
